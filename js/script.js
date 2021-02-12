73107a1c270614ef3973aee20508ad87e859e62b
Krzysztof Rajnert <krzysiekrajnert@gmail.com>
2021-02-12 20:19:20 +0100

    Changed functions setGamePoints & chceckGameWinner for const

--- js/script.js
+++ js/script.js
@@ -22,6 +22,45 @@ function playGame (playerInput) {
 	  printMessage('Nie znam ruchu o id ' + argMoveId + '.');
 	  return 'nieznany ruch';
 	}
+	// Liczenie wyniku
+
+	const setGamePoints = function () {
+		if(computerMove == 'kamień' && playerMove == 'nożyce'){
+			computerScore++;
+		} else if (computerMove == 'nożyce' && playerMove == 'papier') {
+			computerScore++;
+		} else if (computerMove == 'papier' && playerMove == 'kamień') {
+			computerScore++;
+		} else if ( playerMove == 'kamień' && computerMove == 'nożyce'){
+			playerScore++;
+		} else if (playerMove == 'nożyce' && computerMove == 'papier') {
+			playerScore++;
+		} else if (playerMove == 'papier' && computerMove == 'kamień') {
+			playerScore++;
+		}
+		playerPick.innerHTML = "Player: " + playerScore;
+		computerPick.innerHTML = "Computer: " + computerScore;
+	}
+	const checkGameWinner = function () {
+		if (playerScore == 10) {
+			alert('Chyba oszukiwałeś, bo wygrałeś całą grę :-|');
+			alert('Zaczynamy od nowa');
+			playerScore = 0;
+			computerScore = 0;
+			computerMove = "";
+    		playerMove = "";
+			setGamePoints();
+		} else if (computerScore == 10) {
+			alert('Hah wygałem grę!');
+			alert('Zaczynamy od nowa');
+			playerScore = 0;
+			computerScore = 0;
+			computerMove = "";
+    		playerMove = "";
+			setGamePoints();
+		}
+	}	
+
 	// Funkcjca wyświetlania wyniku
 	function displayResult (computerMove, playerMove) {
 		printMessage('Zagrałem ' + computerMove + ', a Ty graczu zagrałeś ' + playerMove + '!');
@@ -58,48 +97,6 @@ function playGame (playerInput) {
 	let playerMove = getMoveName(playerInput);
 
 	displayResult(computerMove, playerMove);
-
-// Liczenie wyniku
-
-		function setGamePoints () {
-			if(computerMove == 'kamień' && playerMove == 'nożyce'){
-				computerScore++;
-			} else if (computerMove == 'nożyce' && playerMove == 'papier') {
-				computerScore++;
-			} else if (computerMove == 'papier' && playerMove == 'kamień') {
-				computerScore++;
-			} else if ( playerMove == 'kamień' && computerMove == 'nożyce'){
-				playerScore++;
-			} else if (playerMove == 'nożyce' && computerMove == 'papier') {
-				playerScore++;
-			} else if (playerMove == 'papier' && computerMove == 'kamień') {
-				playerScore++;
-			}
-			playerPick.innerHTML = "Player: " + playerScore;
-			computerPick.innerHTML = "Computer: " + computerScore;
-		}
-
-		function checkGameWinner () {
-			if (playerScore == 10) {
-				alert('Chyba oszukiwałeś, bo wygrałeś całą grę :-|');
-				alert('Zaczynamy od nowa');
-				playerScore = 0;
-				computerScore = 0;
-				computerMove = "";
-        		playerMove = "";
-				setGamePoints();
-			} else if (computerScore == 10) {
-				alert('Hah wygałem grę!');
-				alert('Zaczynamy od nowa');
-				playerScore = 0;
-				computerScore = 0;
-				computerMove = "";
-        		playerMove = "";
-				setGamePoints();
-			}
-		}	
-
-
 }
 document.getElementById('play-rock').addEventListener('click', function(){
   	playGame(1);
